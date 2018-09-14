53ee12e5f986788356fd16c2b5287abf8e18546c
Clara Mohri <cmohri@stuy.edu>
2018-09-14 07:43:51 -0400

    Update and rename hw6.py to weighted.py

--- 03_occupations/weighted.py
+++ 03_occupations/weighted.py
@@ -1,8 +1,8 @@
-#Team Yellow
-#Clara Mohri, Daniel Keriazis
-#SoftDev1 pd07
-#K06 --StI/O: Divine your Destiny!
-#2018-09-14
+# Team Yellow
+# Clara Mohri, Daniel Keriazis
+# SoftDev1 pd07
+# K06 --StI/O: Divine your Destiny!
+# 2018-09-14
 
 from random import random
 
@@ -30,39 +30,41 @@ def parse_csv_row(csv_row):
 # Adds the csv row `row` into `weights`
 # using entry 1 as a weight and entry 0 as a value
 def add_weight(weights, row):
-    weights[float(row[1])] = row[0]
+    weights[row[0]] = float(row[1])
 
 # Has an `n` chance of returning True, where `n` is a float
 def chance(n):
     return random() < n
 
-# Returns a value of `weights`
-# using its corresponding key as a percentage
-def weighted_choice(weights): # THIS CODE WILL NOT WORK (I think)
-    max_weight = 100
-    for weight in weights:
-        if chance(weight / max_weight):
-            return weights[weight]
-        else:
-            max_weight -= weight
-    return None # If percentages don't add up to 100
-
 # Converts `csv` to a weighted list
 def csv_to_weighted_list(csv):
-    lines = csv.split('\n')
+    # First strip all whitespace then split to get the lines
+    lines = csv.strip().split('\n')
     # Remove the first and last lines,
     # which are a header and total, respectively
-    lines = lines[1:-2]
+    lines = lines[1:-1]
     weights = {}
     for line in lines:
         row = parse_csv_row(line)
         add_weight(weights, row)
     return weights
 
+# Returns a value of `weights`
+# using its corresponding key as a percentage
+def weighted_choice(weights):
+    remaining_weight = 100
+    for val in weights:
+        weight = weights[val]
+        if chance(weight / remaining_weight):
+            return val
+        else:
+            remaining_weight -= weight
+    return None # If percentages don't add up to 100
+
 # Read 'occupations.csv' and return an occupation based on its weight
 def main():
-    
-    occupations = read_file("occupations.csv")
-    print(weighted_choice(csv_to_weighted_list(occupations)))
+    occupations = read_file('occupations.csv')
+    weighted_list = csv_to_weighted_list(occupations)
+    print(weighted_choice(weighted_list))
 
 main()
